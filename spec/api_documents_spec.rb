2918554bb761291438b7107c9eccbcf197e9a680
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-04-10 23:27:02 +0800

    rubocop format

--- spec/api_documents_spec.rb
+++ spec/api_documents_spec.rb
@@ -14,34 +14,26 @@ describe 'Test Document Handling' do
   end
 
   it 'HAPPY: should be able to get list of all documents related with a property' do
-    
   end
 
   it 'HAPPY: should be able to get details of a single document related with a property' do
-    
   end
 
   it 'SAD: should return 404 if document requested is not related with the property indicated' do
-    
   end
 
   it 'HAPPY: should be able to create new documents' do
-    
   end
 
   it 'HAPPY: should be able to delete existing document' do
-    
   end
 
   it 'SAD: should return 404 when try to delete a document that doesnt exists' do
-    
   end
 
   it 'HAPPY: should be able to update existing document' do
-    
   end
 
   it 'SAD: should return 404 when try to update a document that doesnt exists' do
-    
   end
 end
--- spec/api_properties_spec.rb
+++ spec/api_properties_spec.rb
@@ -10,34 +10,26 @@ describe 'Test Property Handling' do
   end
 
   it 'HAPPY: should be able to get list of all properties' do
-    
   end
 
   it 'HAPPY: should be able to get details of a single property' do
-    
   end
 
   it 'SAD: should return error if unknown property requested' do
-    
   end
 
   it 'HAPPY: should be able to create new property' do
-    
   end
 
   it 'HAPPY: should be able to delete existing property' do
-    
   end
 
   it 'SAD: should return 404 when try to delete a property that doesnt exists' do
-    
   end
 
   it 'HAPPY: should be able to update existing property' do
-    
   end
 
   it 'SAD: should return 404 when try to update a property that doesnt exists' do
-    
   end
 end
