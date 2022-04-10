0303ed33cd4de7916764a8d36920ea800bddf4b4
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-04-10 21:49:13 +0800

    property migration

--- app/db/migrations/001_properties_create.rb
+++ app/db/migrations/001_properties_create.rb
@@ -1,1 +1,17 @@
-# TODO Cesar
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:projects) do
+      primary_key :id
+
+      String :name, unique: true, null: false
+      String :description, unique: false, default: ''
+
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
