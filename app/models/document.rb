f8f1f46c1b7f634ba9da3e6a2243b69e91a487e3
sarunyu.sst <sarunyu.sst@gmail.com>
2022-04-18 00:02:45 +0800

    Feature: Implemented column encryption and decryption

--- app/models/document.rb
+++ app/models/document.rb
@@ -10,8 +10,6 @@ module ETestament
 
     plugin :uuid, field: :id
     plugin :timestamps
-    plugin :whitelist_security
-    set_allowed_columns :file_name, :relative_path, :description, :content
 
     # Secure getters and setters
     def description
--- app/models/property.rb
+++ app/models/property.rb
@@ -13,8 +13,6 @@ module ETestament
 
     plugin :uuid, field: :id
     plugin :timestamps
-    plugin :whitelist_security
-    set_allowed_columns :name, :description
 
     # rubocop:disable Metrics/MethodLength
     def to_json(options = {})
