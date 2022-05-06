91744533b329d5b8ffb0cdbf9f92ce50a49f1bbc
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-05-06 04:32:46 -0400

    file formating

--- app/services/add_heir_to_property.rb
+++ app/services/add_heir_to_property.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
 module ETestament
-    # Add a heir to another owner's existing property
-    class AddHeirToProperty
-      # Error for owner cannot be heir
-      class OwnerNotHeirError < StandardError
-        def message = 'The heir cannot be the owner of a property'
-      end
-  
-      def self.call(email:, property_id:)
-        heir = Heir.first(email:)
-        property = Property.first(id: property_id)
-        raise(OwnerNotHeirError) if property.owner.email == heir.email
-  
-        property.add_heir(heir)
-      end
+  # Add a heir to another owner's existing property
+  class AddHeirToProperty
+    # Error for owner cannot be heir
+    class OwnerNotHeirError < StandardError
+      def message = 'The heir cannot be the owner of a property'
+    end
+
+    def self.call(email:, property_id:)
+      heir = Heir.first(email:)
+      property = Property.first(id: property_id)
+      raise(OwnerNotHeirError) if property.owner.email == heir.email
+
+      property.add_heir(heir)
     end
   end
 end
