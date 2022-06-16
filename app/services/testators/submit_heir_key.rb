5bb8cb4780994f17be9fbb36b9a7ae2b95a14829
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-06-16 19:23:57 +0800

    validation

--- app/services/testators/submit_heir_key.rb
+++ app/services/testators/submit_heir_key.rb
@@ -9,7 +9,7 @@ module ETestament
           heir = Heir.first(id: heir_id)
 
           raise Exceptions::BadRequestError, 'Not heir found' if heir.nil?
-          raise Exceptions::BadRequestError, 'Wrong key sent' unless key_content_submitted == heir.key_content_submitted
+          raise Exceptions::BadRequestError, 'Wrong key sent' unless heir.individual_key?(key_content_submitted)
 
           heir.update(key_content_submitted:)
           heir
