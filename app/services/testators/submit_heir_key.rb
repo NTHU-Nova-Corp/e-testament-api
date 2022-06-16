02c189ddded4606216cdbdf9212c7c0524ee0b64
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-06-16 18:18:36 +0800

    key submission validation

--- app/services/testators/submit_heir_key.rb
+++ app/services/testators/submit_heir_key.rb
@@ -6,11 +6,10 @@ module ETestament
       # Validates and stores the key submitted
       class SubmitHeirKey
         def self.call(heir_id:, key_content_submitted:)
-          # TODO: Validate that the key store is the same as the one hashed and stored
-
           heir = Heir.first(id: heir_id)
 
-          raise Exceptions::NotFoundError, 'Not heir found' if heir.nil?
+          raise Exceptions::BadRequestError, 'Not heir found' if heir.nil?
+          raise Exceptions::BadRequestError, 'Wrong key sent' unless key_content_submitted == heir.key_content_submitted
 
           heir.update(key_content_submitted:)
           heir
