d637d438af749e1cea2ac0b8a63975df1e33fd69
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-06-16 13:14:29 +0800

    error 500 fixed

--- app/services/testators/get_request_testator.rb
+++ app/services/testators/get_request_testator.rb
@@ -6,7 +6,11 @@ module ETestament
       # Service object to get pending request for being an executor
       class GetRequestTestator
         def self.call(id:)
-          Account.first(id:).executor_request_received.first.testator_account
+          if Account.first(id:).executor_request_received.first.nil?
+            {}
+          else
+            Account.first(id:).executor_request_received.first.testator_account
+          end
         end
       end
     end
